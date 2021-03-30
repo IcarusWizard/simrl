5492b7436979c4930516fb23a89d1e2515ab6cf2
Icarus <wizardicarus@gmail.com>
2021-03-31 00:26:43 +0800

    fix pickle error

--- simrl/utils/envs.py
+++ simrl/utils/envs.py
@@ -16,54 +16,64 @@ def make_env(config) -> gym.Env:
     return env
 
 class ActionRepeat:
+    def __init__(self, env, amount):
+        self._env = env
+        self._amount = amount
 
-  def __init__(self, env, amount):
-    self._env = env
-    self._amount = amount
+    def __getattr__(self, name):
+        return getattr(self._env, name)
 
-  def __getattr__(self, name):
-    return getattr(self._env, name)
+    def __getstate__(self):
+        return vars(self)
 
-  def step(self, action):
-    done = False
-    total_reward = 0
-    current_step = 0
-    while current_step < self._amount and not done:
-      obs, reward, done, info = self._env.step(action)
-      total_reward += reward
-      current_step += 1
-    return obs, total_reward, done, info
+    def __setstate__(self, state):
+        vars(self).update(state)
+
+    def step(self, action):
+        done = False
+        total_reward = 0
+        current_step = 0
+        while current_step < self._amount and not done:
+            obs, reward, done, info = self._env.step(action)
+            total_reward += reward
+            current_step += 1
+        return obs, total_reward, done, info
 
 class OneHotAction:
+    def __init__(self, env):
+        assert isinstance(env.action_space, gym.spaces.Discrete)
+        self._env = env
 
-  def __init__(self, env):
-    assert isinstance(env.action_space, gym.spaces.Discrete)
-    self._env = env
+    def __getattr__(self, name):
+        return getattr(self._env, name)
 
-  def __getattr__(self, name):
-    return getattr(self._env, name)
+    def __getstate__(self):
+        return vars(self)
 
-  @property
-  def action_space(self):
-    shape = (self._env.action_space.n,)
-    space = gym.spaces.Box(low=0, high=1, shape=shape, dtype=np.float32)
-    space.sample = self._sample_action
-    return space
+    def __setstate__(self, state):
+        vars(self).update(state)
 
-  def step(self, action):
-    index = np.argmax(action).astype(int)
-    reference = np.zeros_like(action)
-    reference[index] = 1
-    if not np.allclose(reference, action):
-      raise ValueError(f'Invalid one-hot action:\n{action}')
-    return self._env.step(index)
+    @property
+    def action_space(self):
+        shape = (self._env.action_space.n,)
+        space = gym.spaces.Box(low=0, high=1, shape=shape, dtype=np.float32)
+        space.sample = self._sample_action
+        return space
 
-  def reset(self):
-    return self._env.reset()
+    def step(self, action):
+        index = np.argmax(action).astype(int)
+        reference = np.zeros_like(action)
+        reference[index] = 1
+        if not np.allclose(reference, action):
+            raise ValueError(f'Invalid one-hot action:\n{action}')
+        return self._env.step(index)
 
-  def _sample_action(self):
-    actions = self._env.action_space.n
-    index = self._random.randint(0, actions)
-    reference = np.zeros(actions, dtype=np.float32)
-    reference[index] = 1.0
-    return reference
+    def reset(self):
+        return self._env.reset()
+
+    def _sample_action(self):
+        actions = self._env.action_space.n
+        index = self._random.randint(0, actions)
+        reference = np.zeros(actions, dtype=np.float32)
+        reference[index] = 1.0
+        return reference
